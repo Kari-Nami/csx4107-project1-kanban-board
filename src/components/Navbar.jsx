3a27c3c34520bab744401f3e4b5da87e2bce3a8d
kari-nami <kazakova.katya@outlook.com>
2026-09-01 17:32:20 +0700

    initial architecture

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function App(props) {
+function Navbar(props) {
   return (
     <div></div>
   );
 }
 
-export default App;
+export default Navbar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function App(props) {
+function Dashboard(props) {
   return (
     <div></div>
   );
 }
 
-export default App;
+export default Dashboard;
