d81ed9330b58f1340324dd6117b784db7720d84a
kari-nami <kazakova.katya@outlook.com>
2026-09-01 18:17:26 +0700

    added working navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import '../App.css'
+import HomeIcon from '@mui/icons-material/Home';
+import { useNavigate } from 'react-router-dom'
 
-function Navbar(props) {
+function Navbar() {
+  const navigate = useNavigate()
+
   return (
     <div className='nav-bar'>
       <h1>Project 1</h1>
+      <button onClick={() => navigate('/')} >
+        <HomeIcon/>
+      </button>
     </div>
   );
 }
