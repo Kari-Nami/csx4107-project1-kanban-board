0f199e8cb5f8b3cdeefe89fa3a615f38d713123b
kari-nami <kazakova.katya@outlook.com>
2026-09-01 22:35:14 +0700

    temp dash to board navigation

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from "../components/Navbar.jsx";
+import { useNavigate } from 'react-router-dom'
 
-function Dashboard(props) {
+function Dashboard() {
+  const navigate = useNavigate()
+
   return (
     <>
       <Navbar/>
+      <button onClick={() => navigate('/board')} >
+        go to board
+      </button>
     </>
   );
 }
