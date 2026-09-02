4e81e98dbf1a32581a51091d756a0ed114e2002f
sasta-kro <sai.aike.shwe.tun.aung@gmail.com>
2026-09-02 16:52:10 +0700

    tasks added

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 
-function Task(props) {
+/* task structure:
+    id: 1,
+    title: "test1",
+    responsible: {
+      id: 12345,
+      name: "Kate"
+    },
+    description: "this is a test task",
+    category: "food",
+    startDate: "01/09/2026",
+    endDate: "03/09/2026",
+    completeDate: null,
+    status: "TO DO"
+*/
+
+function Task({ details}) {
   return (
-    <div></div>
+    <div className='task' key={details.id}>
+      <div className='task-header'>
+        <h3>{details.title}</h3>
+        <div className='category-tag'>{details.category}</div>
+      </div>
+    </div>
   );
 }
 
