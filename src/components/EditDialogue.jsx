839c7ba95bc05bfc81e84ce82f3ac90e7c7974b7
kari-nami <kazakova.katya@outlook.com>
2026-09-04 13:54:32 +0700

    edit dialogue done

--- src/components/EditDialogue.jsx
+++ src/components/EditDialogue.jsx
@@ -1,5 +1,9 @@
 import React, {useEffect, useRef, useState} from 'react';
 import { Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, MenuItem, Select, TextField, Box } from "@mui/material";
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
 import {useTasks} from "../context/TaskContext.jsx";
 import {useCategories} from "../context/CategoryContext.jsx";
 
@@ -36,10 +40,11 @@ function EditDialogue({ task, setSearchParams }) {
   }
 
   return (
-    <Dialog open={Boolean(task)}>
-      <DialogTitle>Edit {task.title}</DialogTitle>
-      <DialogContent>
-        <form id='edit-form' onSubmit={handleEditSubmit}>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Dialog open={Boolean(task)}>
+        <DialogTitle>Editing {task.title}</DialogTitle>
+        <DialogContent>
+          <form  id='edit-form' onSubmit={handleEditSubmit}>
           <TextField margin='dense' variant='outlined' size='small' label='Title' fullWidth
                      value={title} onChange={(e) => setTitle(e.target.value)}
           />
@@ -47,19 +52,6 @@ function EditDialogue({ task, setSearchParams }) {
           <TextField margin='dense' variant='outlined' size='small' label='Description' fullWidth multiline rows={3}
                      value={description} onChange={(e) => setDescription(e.target.value)}
           />
-
-          <FormControl margin='dense' fullWidth>
-            <InputLabel>Category</InputLabel>
-            <Select id='select-category' variant='outlined' size='small' label='Category'
-                    value={category} onChange={(e) => setCategory(e.target.value)}
-            >
-              {categories.map((categoryChoice) => {
-                return (
-                  <MenuItem value={categoryChoice.id} >{categoryChoice.name}</MenuItem>
-                )
-              })}
-            </Select>
-          </FormControl>
 
           <Box className='responsible-inputs-container'>
             <TextField margin='dense' variant='outlined' size='small' label='Responsible Name'
@@ -70,8 +62,25 @@ function EditDialogue({ task, setSearchParams }) {
             />
           </Box>
 
+          <Box className='form-row-container'>
+            <FormControl margin='dense'>
+              <InputLabel>Category</InputLabel>
+              <Select id='select-category' variant='outlined' size='small' label='Category'
+                      value={category} onChange={(e) => setCategory(e.target.value)}
+              >
+                {categories.map((categoryChoice) => {
+                  return (
+                    <MenuItem value={categoryChoice.id} >{categoryChoice.name}</MenuItem>
+                  )
+                })}
+              </Select>
+            </FormControl>
 
-          <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)}/>
+            <DatePicker
+              label="End Date" slotProps={{ textField: { margin: 'dense', variant: 'outlined', size: 'small' } }}
+              value={dayjs(endDate)} onChange={(newDate) => setEndDate(newDate ? newDate.format('DD/MM/YYYY') : '')}
+            />
+          </Box>
         </form>
       </DialogContent>
 
@@ -79,7 +88,8 @@ function EditDialogue({ task, setSearchParams }) {
         <button className='secondary-button' onClick={() => setSearchParams({})}>Cancel</button>
         <button className='primary-button' type={"submit"} form='edit-form'>Save</button>
       </DialogActions>
-    </Dialog>
+      </Dialog>
+    </LocalizationProvider>
   );
 }
 
